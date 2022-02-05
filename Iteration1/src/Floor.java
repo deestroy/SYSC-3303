2206ee6d3e0257a730f082fcd7c9aef027ef1d14
mselim6022 <mohamedselim@cmail.carleton.ca>
2022-02-05 15:38:51 -0500

    Floor records button presses in recentPress

--- Iteration1/src/Floor.java
+++ Iteration1/src/Floor.java
@@ -8,22 +8,24 @@ import java.util.Scanner;
  * Takes the information in table and processes it
  */
 /**
- * @author dhritiaravind
+ * @author Mohamed Selim
  *
  */
-public class Floor {
+public class Floor implements Runnable{
 	private boolean upPressed = false;
 	private boolean downPressed = false;	//direction of travel
 	private int floorNumber;
-	private boolean lampOn = false;			//checks if floor is ready to receive an elevator
-	private boolean arrival;
+	private boolean lampOn = true;			//checks if floor is ready to receive an elevator
 	private ButtonPress recentPress;
 	
 	//private String fileName = "/Users/dhritiaravind/git/SYSC-3303/Iteration1/testdata.txt"; //current location for the file
 	
-	
 	public Floor(int FloorNumber) {
 		this.floorNumber = FloorNumber;
+	}
+	
+	public void run() {
+		
 	}
 	
 	public void pressUp() {
@@ -46,39 +48,7 @@ public class Floor {
 	public int getFloorNumber() {
 		return this.floorNumber;
 	}
-	
-	
-//	/**
-//	 * Parse the data recieved in the input file
-//	 * 
-//	 * @param fileName - the name of the input file assuming it is .txt
-//	 */
-//	public void parseData(String fileName) {
-//		this.fileName = fileName;
-//		File file = new File(fileName);
-//		
-//		
-//		// Referenced from: https://www.geeksforgeeks.org/different-ways-reading-text-file-java/
-//		try {
-//			Scanner sc = new Scanner(file);
-//			while (sc.hasNextLine()) {
-//				// System.out.println(sc.nextLine()); //02:9:23.101 2 Down 1 \n 14:05:15.0 2 Up 4
-//				String data = sc.nextLine();
-//				
-//				// create a new ArrayList with the data seperated
-//				
-//				
-//			}
-//			
-//		} catch (FileNotFoundException e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		}
-//		
-//	}
-	
-	public static void main(String[] args) {
-		Floor f = new Floor();
+	public ButtonPress getRecentPress() {
+		return this.recentPress;
 	}
-	
 }
