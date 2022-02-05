79bef91e4c000b7be0a2d348a9dcd9650949870a
mselim6022 <mohamedselim@cmail.carleton.ca>
2022-02-05 15:01:23 -0500

    Tentative ButtonPress

--- Iteration1/src/ButtonPress.java
+++ Iteration1/src/ButtonPress.java
@@ -2,11 +2,13 @@
  * 
  */
 package src;
-
+import java.time.LocalTime;
 /**
  * @author moham
  *
  */
 public abstract class ButtonPress {
-	
+	private boolean buttonDirection;
+	private int floorNumber;
+	private LocalTime currTime = LocalTime.now();
 }
