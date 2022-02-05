8d521473cb2a499a900e6f586726d50a31d99d7c
deestroy <dhritiaravind@gmail.com>
2022-02-05 13:19:15 -0500

    TODO: implement parse data and maybe move it to floor subsystem

---
+++ Iteration1/src/Floor.java
@@ -0,0 +1,35 @@
+package src;
+import java.time.LocalTime;
+/*
+ * Basic Data about current status of floor
+ * Takes the information in table and processes it
+ */
+/**
+ * @author dhritiaravind
+ *
+ */
+public class Floor {
+	private LocalTime timeL; // = LocalTime.now();
+	private boolean upPressed;
+	private boolean downPressed;	//direction of travel
+	private int initalFloor; 		//user's current floor
+	private int finalFloor;			//user's Destination floor
+	private boolean lampOn;			//checks if floor is ready to receive an elevator
+	private boolean arrival;
+	
+	
+	public Floor() {
+		// parseData();
+		
+	}
+	
+	/**
+	 * Parse the data recieved in the input file
+	 * 
+	 * @param fileName - the name of the input file
+	 */
+	public void parseData(String fileName) {
+		
+		
+	}
+}
