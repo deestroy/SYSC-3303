b8d2e0a56ac0ab813b8cebac4a1f1d230148dbc9
deestroy <dhritiaravind@gmail.com>
2022-02-05 12:25:38 -0500

    Changed around the names of the classes to make it more specific

---
+++ Iteration1/src/ElevatorSubsystem.java
@@ -0,0 +1,11 @@
+package src;
+/* Defines a single Elevator
+ * */
+
+public class ElevatorSubsystem {
+	private boolean light;
+	private boolean button;
+	private boolean doors;
+	private boolean motor;
+
+}
---
+++ Iteration1/src/FloorSubsystem.java
@@ -0,0 +1,15 @@
+package src;
+/* Client in system
+ * 
+ * To read events in the format: Time, floor or elevator number, and button,
+ * Each line of input is to be sent to the Scheduler
+ * */
+public class FloorSubsystem extends Thread {
+//	private boolean light;
+//	private boolean button;
+//	private boolean doors;
+//	private boolean motor;
+	
+
+
+}
