96539b24bd4b5719c988b9c6397e15ce496759e8
mselim6022 <mohamedselim@cmail.carleton.ca>
2022-02-05 15:07:28 -0500

    ButtonPress done other than date/time conversion

--- Iteration1/src/ButtonPress.java
+++ Iteration1/src/ButtonPress.java
@@ -11,4 +11,16 @@ public abstract class ButtonPress {
 	private boolean buttonDirection;
 	private int floorNumber;
 	private LocalTime currTime = LocalTime.now();
+	
+	
+	
+public boolean getButtonDirection() {
+	return this.buttonDirection;
+}
+public int getFloorNumber() {
+	return this.floorNumber;
+}
+public String getPressTime() { //deestroyed can do date/time conversion
+	return "";
+}
 }
