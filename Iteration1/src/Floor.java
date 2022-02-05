338df517b65015c5ce692051122a849c356a1771
mselim6022 <mohamedselim@cmail.carleton.ca>
2022-02-05 14:49:20 -0500

    yyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyy

--- Iteration1/src/Floor.java
+++ Iteration1/src/Floor.java
@@ -12,50 +12,47 @@ import java.util.Scanner;
  *
  */
 public class Floor {
-	private LocalTime timeL; // = LocalTime.now();
-	private boolean upPressed;
-	private boolean downPressed;	//direction of travel
-	private int initalFloor; 		//user's current floor
-	private int finalFloor;			//user's Destination floor
-	private boolean lampOn;			//checks if floor is ready to receive an elevator
+	private boolean upPressed = false;
+	private boolean downPressed = false;	//direction of travel
+	private int floorNumber;
+	private boolean lampOn = false;			//checks if floor is ready to receive an elevator
 	private boolean arrival;
 	
-	private String fileName = "/Users/dhritiaravind/git/SYSC-3303/Iteration1/testdata.txt"; //current location for the file
+	//private String fileName = "/Users/dhritiaravind/git/SYSC-3303/Iteration1/testdata.txt"; //current location for the file
 	
 	
-	public Floor() {
-		parseData(fileName);
-		
+	public Floor(int FloorNumber) {
+		this.floorNumber = FloorNumber;
 	}
 	
-	/**
-	 * Parse the data recieved in the input file
-	 * 
-	 * @param fileName - the name of the input file assuming it is .txt
-	 */
-	public void parseData(String fileName) {
-		this.fileName = fileName;
-		File file = new File(fileName);
-		
-		
-		// Referenced from: https://www.geeksforgeeks.org/different-ways-reading-text-file-java/
-		try {
-			Scanner sc = new Scanner(file);
-			while (sc.hasNextLine()) {
-				// System.out.println(sc.nextLine()); //02:9:23.101 2 Down 1 \n 14:05:15.0 2 Up 4
-				String data = sc.nextLine();
-				
-				// create a new ArrayList with the data seperated
-				
-				
-			}
-			
-		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
-	}
+//	/**
+//	 * Parse the data recieved in the input file
+//	 * 
+//	 * @param fileName - the name of the input file assuming it is .txt
+//	 */
+//	public void parseData(String fileName) {
+//		this.fileName = fileName;
+//		File file = new File(fileName);
+//		
+//		
+//		// Referenced from: https://www.geeksforgeeks.org/different-ways-reading-text-file-java/
+//		try {
+//			Scanner sc = new Scanner(file);
+//			while (sc.hasNextLine()) {
+//				// System.out.println(sc.nextLine()); //02:9:23.101 2 Down 1 \n 14:05:15.0 2 Up 4
+//				String data = sc.nextLine();
+//				
+//				// create a new ArrayList with the data seperated
+//				
+//				
+//			}
+//			
+//		} catch (FileNotFoundException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
+//		
+//	}
 	
 	public static void main(String[] args) {
 		Floor f = new Floor();
