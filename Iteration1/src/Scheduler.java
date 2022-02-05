9462b61be012b3d778503f3be056bc1a545e9024
deestroy <dhritiaravind@gmail.com>
2022-02-05 12:26:56 -0500

    Hopefully should delete Elevator and Floor

--- Iteration1/src/Scheduler.java
+++ Iteration1/src/Scheduler.java
@@ -1,5 +1,11 @@
 package src;
-
+/*
+ *  Scheduler will reply to the Elevator when there is work to be done
+ *  - After data is sent from to the elevator, it will forward the Data to the floor
+ *  
+ *  Iteration 1:
+ *  Scheduler is only being used as a communication channel from the Floor thread to the Elevator thread and back again
+ */
 public class Scheduler {
 
 }
