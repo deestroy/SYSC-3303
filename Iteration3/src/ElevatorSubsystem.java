361014e9166b4c3eebae90e1e08b52acc1e89ab8
Mohamed Selim <97752834+mselim6022@users.noreply.github.com>
2022-03-27 10:02:48 -0400

    Update ElevatorSubsystem.java
    Same changes as elevator.java. this one has skeleton functions for stuff to do. would be great if we could do the same for other classes.

--- Iteration3/src/ElevatorSubsystem.java
+++ Iteration3/src/ElevatorSubsystem.java
@@ -1,10 +1,7 @@
-import java.io.IOException;
-import java.net.DatagramPacket;
-import java.net.DatagramSocket;
-import java.net.SocketException;
+
 import java.time.LocalTime;
 import java.util.ArrayList;
-
+import java.net.*;
 /* Client in system
  * THIS CLASS IS A PLACEHOLDER FOR FUTURE MILESTONES.
  * Elevators will make calls to the Scheduler
@@ -14,92 +11,72 @@ import java.util.ArrayList;
 handle more than one car at a time.  
 
  * */
-public class ElevatorSubsystem implements Runnable {
-	DatagramPacket sendPacket, receivePacket;
-	DatagramSocket sendSocket, receiveSocket;
+
+
+public class ElevatorSubsystem{
 	private Elevator[] cars = new Elevator[1];
-	private Scheduler sched;
-	private ButtonPress recentPress;
+	private DatagramPacket receivedTask, elevatorInfo;
+	private DatagramSocket sendSocket, receiveSocket;
+
 	private ArrayList<ButtonPress> taskList = new ArrayList<ButtonPress>();
-	private ArrayList<ButtonPress> responseList = new ArrayList<ButtonPress>();
 
-	public ElevatorSubsystem(Scheduler sched) {
+	public String elevatorInfo(int elevatorID) {
+		return cars[elevatorID].toString();
+	}
+
+	public ElevatorSubsystem() {
+		this.cars[0] = new Elevator(0, this);
 		try {
-			// Construct a datagram socket and bind it to any available
-			// port on the local host machine.
-			sendSocket = new DatagramSocket(304);
-
-			// Construct a datagram socket and bind it to port 301
-			// on the local host machine..
-			receiveSocket = new DatagramSocket(303);
-
+			sendSocket = new DatagramSocket();
+			receiveSocket = new DatagramSocket(69);
 		} catch (SocketException se) {
 			se.printStackTrace();
+			System.exit(1);
 		}
-
-		this.sched = sched;
-		this.cars[0] = new Elevator(0, this);
 	}
-
-	public void run() {
+	
+	//
+	public static void main(String[] args) {
 		while (true) {
-			byte first[] = new byte[5];
-			receivePacket = new DatagramPacket(first, first.length);
-
-			try {
-				System.out.println("ElevatorSubsytem Waiting..."); // so we know we're waiting
-				receiveSocket.receive(receivePacket);
-			} catch (IOException e) {
-				System.out.print("IO Exception: likely:");
-				System.out.println("Receive Socket Timed Out.\n" + e);
-				e.printStackTrace();
-			}
-
-			int len = receivePacket.getLength();
-			String received;
-			received = new String(receivePacket.getData(), 0, len);
-			if (received.length() == 1) {
-				/*
-				 * if (this.sched.getQueue().size() == 0) { System.out.println(LocalTime.now() +
-				 * " Scheduler queue is Empty "); try { this.sched.getQueue().wait(); } catch
-				 * (InterruptedException e) { e.printStackTrace(); }
-				 * 
-				 * }
-				 */
-
-				synchronized (this.taskList) {
-					this.taskList.add(this.sched.getQueue().get(0));
-					this.taskList.notifyAll();
-				}
-				this.sched.getQueue().remove(0);
-				System.out.println(LocalTime.now() + " ElevatorSubsystem receieved something! ");
-			}
+			//send status of all elevators to scheduler
+			
+			//Loop:
+			//receive task from scheduler. timeout
+			
+			//send task to the elevator specified
+			
+			//new task? no? timeout
+			
+			//any elevator have something to report?:
+			
+				//send elevator info to scheduler
+			//else: loop
+						
 		}
+			
 	}
 
+	public void receiveFromSched() {
+		
+	}
+	public void sendToElevator() {
+		
+	}
+	public void sendToSched() {
+		
+	}
 	public ArrayList<ButtonPress> getTaskList() {
 		return this.taskList;
 	}
 
-	/**
-	 * Adds button presses to the response queue.
-	 * 
-	 * @param buttonpress, the button that will be added
-	 */
-	public void addResponseList(ButtonPress buttonpress) {
-		synchronized (this.responseList) {
-			this.responseList.add(buttonpress);
-			this.responseList.notifyAll();
-		}
-	}
 
 	/**
 	 * Changes the status of the lamp
 	 * 
 	 * @param direction, direction of the elevator
-	 */
+	 
 	public void changeLampStatus(int direction) {
 		this.sched.changeLampStatus(direction);
 	}
-
+	*/
 }
